341ca3bed2096707e2ae7b3a7ea66f6e3f8d2d92
Mohammedalamry <Mohammedalamry@users.noreply.github.com>
2019-11-12 23:04:58 +0300

    update for js and readme

--- README.md
+++ README.md
@@ -1,56 +1,86 @@
 # Memory Game Project
+ This is a project that is designed and represented by using HTML Hypertext language and CSS to form a webPage and javascript to make webpage interactive with the user who wants to play a game and it's a challenge for people who want to be a web developer.
+there are two Essential aspects of Memory game project:  
+first is static Side of this project this is represented by HTML and CSS 
+the Second its interactive side that is  represented by javascript I  
+I explained the steps to start and build .ect to run the project. in the section that is below the Heading of this page.
+## start to get project
+  I have download the copy of the project from Udacity classroom 
+  that contains the static part files and interactive part file that is to start to build the interactive file and to complete static files that need to run the application. this step as help from audacity because i have joined in web development Nanodgree from Udacity instead of building applications from scratch point.   
 
-## Table of Contents
+## pre requirements for project.
+* First I create an account on GitHub to take the clone of MemoryGame project on my desktop.
+* second I install web browsers such as chrome to run the project.
+* third  I install Editor such as Sublime text 3 to open the files that include in this project and for writing codes to run the application successfully on the web browser.  
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
-*  [startgame](#startgame)
-* [enevtclikonthegame](#enevtclikonthegame)
-## Instructions
+## start step open files of game and take a look in the files.  
+   First, I have opened Sublime text 3 on then I opened the files of the memory Game project in the editor. then I have take look at what is inside these files to understand and plan for a code that I need to run the project.
+Socond I have opened CSS and HTML files these are the static side of the project then i have opened the js file this is the interactive side of the project. inside it, some comments are tell me how to start coding for the game as Guide. to understood and following them to start coding steps. 
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+## Coding steps
+under this topic from the start to the run step of game to End game. 
 
-To get started, open `js/app.js` and start building out the app's functionality
+## Display card on Game Boared  
+in this first step, I learn how to connect HTML and CSS Files with Javascript
+file to make the web page interactive through the Document Object Model - DOM). 
+#### steps perform this task   
+ * First i creat two varaiblies  **Selectcard and card** with their values. 
+  **Selectcard** to get cards class from HTML file by use _querySelectorAll method_. 
+   **card** to store**Selectcard** in the array so that _querySelectorAll method_ return collection of THML. because i need loop over each card to show and open. so that i need to store them at arry.   
+ * Second i creat for of loop .
+ * Thrird I use _The classList Property-
+ with _ add property_ to add th classes that are show and open each card in the _Gameboard_. 
+  ####This is the code to preform Dispaly card on the Game board
+  ```
+     let Selectcards = document.querySelectorAll('.card'); 
+     let card = [...Selectcards];
+     for( let i of card ){ 
+         i.classList.add('open', 'show', 'match','change');
+ }  
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
 
-## Contributing
+     ```
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+## shuffl cards randomly 
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
-## startgame 
-Tish is how you can make your game interactive with CSS and HTML  by use document objects. Frist, therefor I have used document objects to make game Boards without any open, show, and, match cards array to loop over each card so that the first look for the game there is not any card appear and use the classlist method to remove cards. invoke shuffleicons method. 
-## enevtclikonthegame
-
-* [Eventclickonthecards](#Eventclickonthecards)
-* [EvenclickonResetGame](#EvenclickonResetGame)
-* [Evenclickonthetheplayagin](#Evenclickonthetheplayagin)
-
+#### steps perform this task 
+* First creat unored list to hold all crads.
+* scond creat an array that is **icons** to push  all cards inside it 
+* thrid pass **icons** array to_shuffle Method_ 
+* store return random varablie from shuffle arry in the **icons** arry
+* final assign this values of **icons** array to the **card**array. 
  
-## Eventclickonthecards 
-   user can click on the two cards just so that there is if condition(opencard.lengh<2 ) that prevents user from click more than two cards.
-   when the user stars click on the card the first card will open and set the value of cards click to the parameter at funopencards(get) then push card in an array. inside the function of funopencards.  if condition to check .if users click on the same card or different cards.
-   if the card is different that will save in firs1,tiwc1 that will save in card1c,card2c the class name of each card className. other the opencards array will be empty.
+ 
+## Event click on the cards
+  befor I do this steps I roveme all carsd from gameboard.
+#### steps perform this task 
+    Frist add Event Listener to the target cards.  
+    there is if condition(opencard.lengh<2 ) that prevents user from click more than two cards.
+when the user stars click on the card the first card will open and set the value of cards click to the parameter at funopencards(get)
+##  funopencards method
+  create an array to push cards that are clicked. 
+  if condition to check .if users click on the same card or different cards.
+  if the cards are different and `opencard.lengh==2`  then will be ready to be set in the comparing methods that are _ matchcard Method, notmatchcard Method_ and save the class name of each card in card1classname,card1classname.
+  others the opencards array will be empty.
 
-##  matchcardmethod
- this method will check if the class name of the first card == className of the Second cards.
- if firs==twic  that will show match card on the game deck by use classlist property.
- opecards array Empty.  invoke for move counter and startcounthn functions        .
+##  matchcard Method
+  this method will check if the class name of the first card == className of the Second cards. `if(firs==twic)`
+  that will show match cards on the game deck by use _classlist property_.
+ opencards array Empty to be ready to compare new click cards.
+ . invoke for movecounter and startcounthn functions to count the Move and the star rate for the user.
+## notmatchcard Method 
+ This method will check if the class name of the first card != className of second cards.
+ `if firs!=twic ` that will show cards in red card color there is a Setimout method to make cards appear for less than Second then remove it from the game deck by use _classlist property_.
+ opecardas array Empty to be ready to compare new click cards.
+invoke for movecounter and startcounthn functions to count the Move and the star rate for user.
 
-## notmatchcardmethod 
-This method will check if the class name of the first card != className of second cards.
- if firs!=twic  that will  show-cards  in red card color there is a Setimout method to make cards appear for less than Second then remove it from the game deck by use classlist property.
- opecardas array Empty.
-  call for move counter and startcounthn functions.
-
-## EvenclickonResetGame 
+## Even clickon Reset Game 
  if there is a click on reset icon all following will be reset:
-    Move will be set to 0, star rest to three stars .the gameboard remove all card by use loop over an array that holds all cards this is Cards array and use classlist. remove, and clear for opencard array and fris1 and twic1. setTime will be reset to 0 of second and mint and invoke shuffleicons method.  
+    Move will be set to 0, star rest to three stars .the gameboard remove all card by use loop over an array that holds all cards this is **card** array and use classlist. remove, and clear for opencard array and Firstcard and Secondcard. setTime will be reset to 0 of second and mint and invoke shuffleicons method.  
  
-## Evenclickonthetheplayagin
-   The function play again will be available after the function pop message execute 
-   if click on the button that will reset the game and invoke shufllicons method.
+## Event clickon the playagin button
+   The function play again will be available after the function pop message execute.
+   if click on the button that will reset the game.
 ##information of game for user 
 
 
@@ -61,17 +91,18 @@ This method will check if the class name of the first card != className of secon
 ## Timer
     There is setTime function to make the form of time minute and second 
     Setimout function that makes the count of time appear in the HTML Timer Element by use innerHTML
-   the timer runs it dependent on if condition. if secom ==1 then the timer will start and secom it dependent on the first click to start the game.
+   the timer runs it dependent on if condition.if TimeControl==1 then the timer will start and TimeControl it dependent on the first click to start the game.
 
 ## Move 
-   In this method will show  number move for two card click 
- that dependent on the countr++ and invoke function Movecounter in machtcard method and notmachard function and  the opecards function  below  if condition to test if click was in the same cards then set result in the Move HTML element by the Move tag inner HTML on the Webpage. 
+    In this method will show number move for two card click 
+ that depends on the countr++ and invoke function Movecounter in machtcard method and notmachard function. 
 
-## Star
-  In this method will show  number  of stars for two  after number of Movement.  
- that dependent on the countr++ and invoke function startcounthn in machtcard method and notmachcard function and in the opencards function   under if condition to test if click was in the same cards
- then set result in the star HTML element by classlist.add() on the Webpage. 
-
+## Star 
+  In this method will  decreas  number  of stars for two  after number of Movement that is depnds on Movecounter method.   
+## pop messsage method 
+  This  method to pop up information such as Time, rate of star
+   and a number of movements. 
+   set and show options button to play.   
 
 ## Endgame 
   The game will end if mactchcard.lenth==16 and timer will stop and remove all matchcard with other
--- js/app.js
+++ js/app.js
@@ -1,42 +1,36 @@
-/*
- * Create a list that holds all of your cards
- */let stars =  document.querySelector('.stars');
-let starsa = [];
-  let s = document.querySelectorAll('.card');
-   
-  let card = [...s];
-   let outicon= [];
-    let x;
-     let poit=0;
-  let count=0;
- let opencards = [];
-    let listcard = document.getElementById('clik');
-    let secom=0;
-    let second =0;  
-     let mint=0;
-    
-
+ 
+let stars =  document.querySelector('.stars'); //this is to get HTML collection of stars.
+let starsa = []; //this is an array to store the star HTML collection. 
+let Selectcards = document.querySelectorAll('.card'); //this is to get HTML collection cards.   
+let card = [...Selectcards];//this array used to control HTML collection cards.
+let count=0;//this is use to count move of user. 
+let opencards = [];//this is use to store opencards after click for matches.
+let listcard = document.getElementById('clik');
+let TimeControl=0; // this is use to control of timer start.
+let second =0;  //this is to count the Second in settimer fucnction.
+let mint=0; //this is to count the mint in settimer fucnction.
+ //these varablies to use in matched to store cards and classname of cards.  
+ let Firstcard,Secondcard,card1classname,card2classname;
+ //=================================================================
+     //revome all the opens and show cards from Gameboord.
   for( let i of card ){ 
          i.classList.remove('open', 'show', 'match','change');
-     
-  
-   }   let firs1,   
-     tiwc1,card1c,card2c;
- 
-   
-//====================================================================   
+ }  
 
+
+//====================================================================   
+     // creat list that holds all cards then push them into icon arry 
          let icons = []; 
   const cardlist = document.createElement('ul');
-     cardlist.classList.add('h');
+      
        let c =0;
      
      for(let i of card ){ 
           
             const newElement = document.createElement('li');
             cardlist.appendChild(newElement);
-          let h = i.children[0];
-          let cards = h.className;
+          let itemscards = i.children[0];
+          let cards = itemscards.className;
           newElement.innerHTML=cards;
             newElement.classList.add('test'); 
            let cardt = cardlist.children[c].innerText; 
@@ -44,126 +38,106 @@ let starsa = [];
          c++;
        
          icons.push(cardt);          
-
-         
-
-   } 
+} 
      
 //======================================================================
-    
-      let list= cardlist;
-function shuffleicons(){                     
+   /**
+* @description this Method use it assaing  the random card that come from shuffl arry to card arry 
+*/  
+ function shuffleicons(){                     
 
      let as= 0;
        icons = shuffle(icons);
-     for(let icon of card){
+     for( let icon of card){
 
-          let carsd = icon.children[0];  
+         let cards = icon.children[0];  
            
-          carsd.className =icons[as];
+          cards.className =icons[as];
           as++;
 
-     } } 
+}   } 
       shuffleicons();
 
 //=======================================================================
+   
+ /**
+* @description tthis Method is used  to respons for the user when click on the cards.
+*/  
 
  function respondToTheClick(evt) { 
     
- 	const h = evt.target;
-        
+  const h = evt.target;
 
- secom=1;
- poit++;
- 	if(h.matches('li')){                                                   
+ TimeControl=1;
+
+  if(h.matches('li')){                                                   
         if (opencards.length<2) { 
- 	                 evt.target.classList.toggle('open');
- 	                 evt.target.classList.toggle('show');
-              console.log('A paragraph was clicked: '+evt.target.children[0].className);}
+        evt.target.classList.toggle('open');
+        evt.target.classList.toggle('show');
+ }
    funopencards(h); 
 
-  }  
+}  
  }
 
- let checkcard= [];             let mark =0;
-
+ 
 //=================================================================
+ //
+
+  /**
+* @description used to push clicked cards into opencards arry and prevent user from selecting the Same cards 
+    
+* @param {array} get  
+*/   
 function funopencards(get) {
-      
-	       mark++;
-
-
-         opencards.push(get); 
-          let d=0; 
-           
- 
+   opencards.push(get); 
+          
 if(opencards[0]==opencards[1]){
-    count++;    movecounter( );         
-  startcounthn();        
-
-               opencards=[];
-           }  
-      else if(opencards[0]!=opencards[1]) {
+    opencards=[];
+ }   else if(opencards[0]!=opencards[1]&&opencards.length==2) {
  
-          firs1 = opencards[0];
-          tiwc1 = opencards[1];
-          if(opencards.length==2){ 
-       card1c=firs1.children[0].className;                  
-       card2c= tiwc1.children[0].className;
+          Firstcard = opencards[0];
+          Secondcard = opencards[1];
         
-         carchecker();
+       card1classname=Firstcard.children[0].className;                  
+       card2classname= Secondcard.children[0].className;
         
-        
-}        }  
-   
-
-    
-
-     
- 
-
-
-   
-      
+      carchecker();       
+       }  
   
-    
-       
-	}                
+           
+  }                
 //============================================================
 
-let jo =document.querySelectorAll('.card.open.show');
-   setTimeout(function go() {
-   if (secom==1) {  second++; }
+
+     setTimeout(function go() {
+   if (TimeControl==1) {  second++; }
    
    setTime(); 
     setTimeout(go,750); 
+ },800);  
 
-  
-     
-  },800);  
 listcard.addEventListener('click', respondToTheClick); 
- let jl =document.querySelectorAll('.card.match');
-  function carchecker () {    
-      matchcard (card1c,card2c) ;
-     notmacthacrd(card1c,card2c) ;
-     popmessage();
-
-
-   
-     
+   function carchecker () {    
+      matchcard (card1classname,card2classname) ;
+     notmacthacrd(card1classname,card2classname) ;
+     popmessage();   
   }  
      
 
 
 
-//=======================================================================	
-
+//======================================================================= 
+ 
+/**
+* @description this is method to show matched card in game Board.
+  @param {string  }  firs this is Classname of Firstcard. 
+  @param {string }  tiwc this is Classname of Secondcard.
+*/ 
  function matchcard (firs,tiwc){                     
   
-    
- 
       if (firs==tiwc) {
-      	
+        
        opencards[0].removeEventListener('click', respondToTheClick);              
          opencards[0].classList.add('match');
          opencards[1].classList.add('match');
@@ -171,71 +145,80 @@ listcard.addEventListener('click', respondToTheClick);
        count++;
        movecounter();
        startcounthn();
-        console.log( firs+'=='+tiwc); 
+        
       }              
           
   }
 
 //===================================================================
-  
-     let chang= document.querySelector('.change');
-    
+/**
+*@description this fuction is used to remove all not matched cards in the game baord.
+*@param {string }  firs this is Classname of Firstcard.
+*@param {string }  tiwc this is Classname of Secondcard.
+*/ 
+    //
     function notmacthacrd (firs,tiwc) {
        
        if(firs!=tiwc){                   
-
-opencards[0].classList.add('change');
+ opencards[0].classList.add('change');
     opencards[1].classList.add('change'); 
-        mark=0;            
- } setTimeout(function remove() {
+                    
+} 
+  setTimeout(function remove() {
 
 if (firs!=tiwc) { 
-       
-      firs1.classList.remove('change');
-       tiwc1.classList.remove('change'); 
-   firs1.classList.remove('open');
-   firs1.classList.remove('show');
-   tiwc1.classList.remove('open');
-   tiwc1.classList.remove('show'); 
+     Firstcard.classList.remove('change');
+    Secondcard.classList.remove('change'); 
+  Firstcard.classList.remove('open');
+  Firstcard.classList.remove('show');
+   Secondcard.classList.remove('open');
+   Secondcard.classList.remove('show'); 
            opencards=[];
        count++; 
        movecounter();
        startcounthn();
-}
+ }
 
-         },500);
-     
-
-      }
+},500);
+        }
          
 
 
 
 //=====================================================================
-     let m = document.querySelector('.pop');
-     let ifno=m.querySelector('.inforate'); 
-     
-   function popmessage(){      
-    mat =document.querySelectorAll('.card.match');
-  if( mat.length==16){ 
+     //these variables are used to control in the  pop message when user win the game 
+     let matcards;
+     let showmessag = document.querySelector('.pop');
+     let ifno=showmessag.querySelector('.inforate'); 
+  
+    /**
+* @description Method to show for user informaitin after winng in game such as time and rate of star ,Timer and give him optino if he want play again. 
+*/ 
+    function popmessage(){ 
+
+    matcards =document.querySelectorAll('.card.match');
+  if( matcards.length==16){ 
            
-     m.style.display= 'flex';
-     ifno.children[0].children[0].innerText=output;
+    showmessag.style.display= 'flex';
+     ifno.children[0].children[0].innerText= outpuTimer;
      ifno.children[1].children[0].innerText=count;
-     ifno.children[2].children[0].innerText=nst;
-     secom=0;
+     ifno.children[2].children[0].innerText=countstar;
+     TimeControl=0;
 
 for( let i of card ){ 
          i.classList.remove('open', 'show', 'match');
-     
- 
-   }
+ }
 
      
      console.log("mmm");
   }   }
 
 //========================================================================
+    //
+
+    /**
+* @description this fuction to count step moves when user click on two cards  .
+*/ 
      let counter;
   function movecounter (){
       counter = document.querySelector('.moves');
@@ -244,44 +227,52 @@ for( let i of card ){
        
 }  
 //=====================================================================
-let mat;
-let stm;
-let nst=3;
+
+ //this varaible use to control of the number of star 
+let countstar=3;
+ 
+   /**
+* @description this method use to decrease the number of star and remove srtar.
+*/ 
 function startcounthn(){
   
-  let clchil;
+  let getstars;
       for(let d=0;starsa.length<3;d++){ 
-           clchil= stars.children[d];
-starsa.push(clchil);
-   stm=starsa[d];       } 
+          getstars= stars.children[d];
+starsa.push(getstars);
+          } 
  
  
  if(count==10){
   
        starsa[2].children[0].classList.remove('fa-star'); 
-              nst=2;
+              countstar=2;
     
- }
-else if (count>12){ 
+ }else if (count>12){ 
  starsa[1].children[0].classList.remove('fa-star');    
 
- nst=1;
-    }  
-else if (count==20){ 
+ countstar=1;
+}  else if (count==20){ 
  starsa[0].children[0].classList.remove('fa-star'); 
-  nst=1;
+ countstar=1;
  
-} 
-
-
- 
+}   
 
 }
  
 
 //=================================================================
+    // this variable use to set time when the game started by user click.
   let ctimer;
-  let output; 
+
+  //this varaible to set final time when pop message appear.
+  let  outpuTimer; 
+
+  
+    /**
+* @description  this method use to Form the Timer .
+*/ 
+ 
 function setTime(){                    
   
     
@@ -291,28 +282,38 @@ let time = document.querySelector('.Timer');
   ctimer = time.children[1];
  if( second<10 ){                             
  ctimer.innerHTML=('0'+mint+':'+'0'+second);
-   }else if (second>9){  ctimer.innerHTML=('0'+mint+':'+second); }
+ }else if (second>9){  
+  ctimer.innerHTML=('0'+mint+':'+second); 
+ }
    if(second>59){
      mint+=1;
     second=0;
-     Sec1=0;
+     
   ctimer.innerHTML=('0'+mint+':'+'0'+second);
        
 }          
    if(mint>9){  
       
-    ctimer.innerHTML=(mint+':'+'0'+second);       } 
+    ctimer.innerHTML=(mint+':'+'0'+second);       
+
+} 
     if (mint>9&&second>9){  ctimer.innerHTML=(mint+':'+second); }
 
-   output = ctimer.innerText
+   outpuTimer = ctimer.innerText;
    
    }
  
   
 
 //==============================================================
+ //this varaible represent restart icon and use it with event click. 
 let restart = document.querySelector('.restart');
 restart.addEventListener('click',resttsart);
+
+/**
+ * @descriptionthis function is used to restart the game and respons for the click on restart icon.
+*/ 
+  
 function resttsart(evt){
  mint=0; second=0;
 count=0;
@@ -325,18 +326,24 @@ count=0;
    } 
 
      //this to  aviod Erreor  when  click one card after that click on ressetbuttn
-   if (opencards.length==1) {firs1=[]; tiwc1=[]; opencards=[]; }
-       
+   if (opencards.length==1) {Firstcard=[]; Secondcard=[]; opencards=[]; }
+ //this is use form time after click on restart icon     
 ctimer.innerHTML= '0'+mint+':'+'0'+second;
-secom=0;
+TimeControl=0;
+//this is use to check in the console panle just
  console.log('msg');
-   
+   //this is to set user move to 0 
    movecounter(0);
-  
-if(nst<3) { 
+
+ //rest the nubember of star to 3 star 
+if(countstar<3) { 
 starsa[1].children[0].classList.add('fa-star'); 
  starsa[2].children[0].classList.add('fa-star'); }
+
+ //invoke shufflle method 
  shuffleicons();
+
+//to remove all macht card when the user click restart icon
 listcard.children[0].children[0].classList.remove('.card.match');
 
 
@@ -346,32 +353,43 @@ listcard.children[0].children[0].classList.remove('.card.match');
 } 
 
 //===============================================================
+////this variable represents a button in the pop message to its className and use it with event click. 
    let play2= document.querySelector('#playagain');
     play2.addEventListener('click',playgin);
+        /**
+* @description this method use to response to button click  after user winning at Memory Game  user and he want to play again .     
+*/ 
+  
  function playgin(evt){
   mint=0; second=0;
 count=0; 
  let play =evt.target;   
   if(play.matches('button')){     
 
-   m.style.display='none';
+  showmessag.style.display='none';
+   //remove  all match cards and 
     for( let i of card ){ 
-         i.classList.remove('open', 'show', 'match');
-    
-   } 
+  i.classList.remove('open', 'show', 'match');
+} 
+ //Reset time to 00:00 and stop Timer
 ctimer.innerHTML=('0'+mint+':'+'0'+second);
-secom=0;
+TimeControl=0;
 // this just to check on the cosole.
- console.log('msg');
+  console.log('msg');
+  //clear opencards array of match card
   opencards=[];
-   movecounter(0);
+  //reset Move of user to 0  
+  movecounter(0);
 
-
- if(nst<3) {                                              
+//rest the nubember of star to 3 star
+ if(countstar<3) {                                              
  starsa[2].children[0].classList.add('fa-star'); 
 starsa[1].children[0].classList.add('fa-star'); 
 }
+//invoke shufflle method 
  shuffleicons();
+
+ //to remove all macht card when the user click playainbutton
 listcard.children[0].children[0].classList.remove('.card.match');
 
 
